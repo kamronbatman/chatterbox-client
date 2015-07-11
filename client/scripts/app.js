b0fa91a5bfc53ff5fbea0c93cd345072f87bb234
MKS-students <mks.students@hackreactor.com>
2015-07-10 17:35:57 -0700

    Basic Requirements completed.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,6 +3,8 @@ var app;
 $(function() {
   app = {
     server: 'https://api.parse.com/1/classes/chatterbox',
+    rooms: new Set(),
+    //currentRoom: 'lobby',
 
     init: function() {
       //Fetch all messages
@@ -10,6 +12,16 @@ $(function() {
       //Display
 
       app.fetch();
+      $('#roomSelect').val('lobby');
+
+      $('#sendInput').on('click', function(){
+        var currentRoom = $('#roomSelect').val();
+        var message = $('#messageInput').val();
+
+        app.send( app.createMessage(currentRoom,message) );
+        app.clearMessages();
+        app.fetch();
+      });
     },
 
     send: function(message) {
@@ -21,6 +33,8 @@ $(function() {
         success: function (data) {
           console.log('chatterbox: Message sent');
           console.log('chatterbox:', data);
+
+          //Fetch all messages
         },
         error: function (data) {
           console.error('chatterbox: Failed to send message');
@@ -33,15 +47,13 @@ $(function() {
       $.ajax({
         url: app.server,
         type: 'GET',
-        //data: JSON.stringify(message),
+        //data: { order: '-createdAt'},
         contentType: 'application/json',
         success: function (data) {
           console.log('chatterbox: Message received');
           console.log('chatterbox:', data);
 
-          if ( 'results' in data ) {
-            _.each(data.results, function(message) { app.addMessage(message) });
-          }
+          _.each(data.results.reverse(), function(message) { app.addMessage(message) });
         },
         error: function (data) {
           console.error('chatterbox: Failed to receive message');
@@ -56,17 +68,31 @@ $(function() {
 
     addMessage: function(message) {
       message = app.clean(message);
-      $('#chats').append(
-          $('<div>').addClass('message').html(
-              '(<strong>' + message.roomname + '</strong>) ' + '<em>' + message.username + '</em>: ' + message.text
-            )
-        );
+
+      if (message.username && message.roomname) {
+        displayed = '(<strong>' + message.roomname + '</strong>) ' + '<em>' + message.username + '</em>: ' + message.text;
+
+        if (message.auth) {
+          if ( app.cryptText( message.roomname, message.username, message.text ) == message.auth3 ){
+            displayed = '<img src=images/star.gif>' + displayed;
+          } else {
+            displayed = '<img src=images/silver-star.gif>' + displayed;
+          }
+        }
+
+        $('<div>').addClass('message').html(displayed).prependTo('#chats');
+
+        //Add Rooms
+        app.addRoom(message.roomname);
+      }
     },
 
     addRoom: function(room) {
-      $('#roomSelect').append(
-        $('<option>').addClass('room').text(room)
-        )
+      room = room.toLowerCase();
+      if (!app.rooms.has(room)) {
+        $('<option>').addClass('room').attr('value',room).text(room).appendTo('#roomSelect');
+        app.rooms.add(room);
+      }
     },
 
     addFriend: function(friend) { },
@@ -77,6 +103,36 @@ $(function() {
       message.text = _.escape(message.text);
 
       return message;
+    },
+
+    getParameter: function(sParam)
+    {
+        var sPageURL = window.location.search.substring(1);
+        var sURLVariables = sPageURL.split('&');
+        for (var i = 0; i < sURLVariables.length; i++)
+        {
+            var sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] == sParam)
+            {
+                return sParameterName[1];
+            }
+        }
+    },
+
+    getUsername: function(){
+      return app.getParameter('username');
+    },
+
+    createMessage: function(roomname,text) {
+      return {  username: app.getUsername(),
+                roomname: roomname,
+                text: text,
+                auth:true,
+                auth3: app.cryptText(roomname,app.getUsername(),text) };
+    },
+
+    cryptText: function(roomname,username,text) {
+      return CryptoJS.SHA256('WE WIN!' + roomname + username + text + 'WE WIN!').toString();
     }
   }
 })
